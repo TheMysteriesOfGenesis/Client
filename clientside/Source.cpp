7f4016b3db0c2b1885d8a9063aaffade75d40f0e
Maurits van Riezen <MauritsVanRiezen@gmail.com>
2016-12-11 12:41:48 +0100

    initial commit

---
+++ clientside/Source.cpp
@@ -0,0 +1,29 @@
+#include "Client.h"
+
+
+int main()
+{
+	system("color 8b");
+	std::cout << "Mysteries of Genesis Opened Successfully" << std::endl;
+	Client myClient("127.0.0.1",1111); //Create client to localhost ("127.0.0.1") on port 1111
+
+	if (!myClient.Connect()) //If client fails to connect...
+	{
+		std::cout << "Failed to connect to server..." << std::endl;
+		system("pause");
+		return 1;
+	}
+	myClient.RequestFile("donald.jpg");
+	std::string userinput; //holds the user's chat message
+	while (true)
+	{
+		std::getline(std::cin,userinput); //Get line if user presses enter and fill the buffer
+		if (!myClient.SendString(userinput)) //Send string: userinput, If string fails to send... (Connection issue)
+			break; //If send string failed (if connection was closed), leave this loop since we have lost connection to the server
+		Sleep(10);
+	}
+
+	system("pause");
+	return 0;
+}
+
